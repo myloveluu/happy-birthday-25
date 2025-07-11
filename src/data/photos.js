696d9acc4f233dfd188ad5535ff8a6e1b87f3db7
Andrew <pold.jacobo89@gmail.com>
2025-07-11 02:33:27 -0500

    Se completó diseño collegue

---
+++ src/data/photos.js
@@ -0,0 +1,254 @@
+export const photos = [
+    {
+        id: 1,
+        src: "/photos/photo1.jpeg",
+        alt: "La primera vez que nos conocimos",
+        title: "La primera vez que nos conocimos",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 2,
+        src: "/photos/photo2.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 3,
+        src: "/photos/photo3.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 4,
+        src: "/photos/photo4.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 5,
+        src: "/photos/photo5.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 6,
+        src: "/photos/photo6.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 7,
+        src: "/photos/photo7.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 8,
+        src: "/photos/photo8.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 9,
+        src: "/photos/photo9.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 10,
+        src: "/photos/photo10.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 11,
+        src: "/photos/photo11.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 12,
+        src: "/photos/photo12.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 13,
+        src: "/photos/photo13.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 14,
+        src: "/photos/photo14.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 15,
+        src: "/photos/photo15.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 16,
+        src: "/photos/photo16.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 17,
+        src: "/photos/photo17.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 18,
+        src: "/photos/photo18.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 19,
+        src: "/photos/photo19.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 20,
+        src: "/photos/photo20.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 21,
+        src: "/photos/photo21.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 22,
+        src: "/photos/photo13.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 23,
+        src: "/photos/photo23.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 24,
+        src: "/photos/photo24.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 25,
+        src: "/photos/photo25.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 26,
+        src: "/photos/photo26.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 27,
+        src: "/photos/photo27.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 29,
+        src: "/photos/photo29.jpg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 34,
+        src: "/photos/photo34.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 35,
+        src: "/photos/photo35.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 36,
+        src: "/photos/photo36.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 37,
+        src: "/photos/photo37.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 38,
+        src: "/photos/photo38.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 39,
+        src: "/photos/photo39.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 40,
+        src: "/photos/photo40.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    },
+    {
+        id: 41,
+        src: "/photos/photo41.jpeg",
+        alt: "Nuestra primera vez en el zoológico",
+        title: "Nuestra primera vez en el zoológico",
+        message: "Un mensaje de prueba"
+    }
+]
